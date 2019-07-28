688e353109e5401b98a0104a922e6c3dfa5e2edb
Iqbal Ahmed <iqbal@iqbal.com>
2019-07-28 16:55:42 +0100

    Add more functionality to util

--- src/util/predictiveWords.js
+++ src/util/predictiveWords.js
@@ -1,17 +1,27 @@
 import words from './words.json'
 
 export const getWords = (inputText) => {
-    if (!inputText){
+    const cleanedInputText = getCleanInput(inputText)
+    
+    if (!cleanedInputText){
         return [];
     }
-    const inputTextCleaned = inputText.toLowerCase().trim();
+    
 
     const wordsFound = words.filter((element) => {
-        return element.word.startsWith(inputTextCleaned)
+        return element.word.startsWith(cleanedInputText)
     })
 
     const sortedByFrequency = wordsFound.sort((a, b) => b.count - a.count );
     
     const top3Words = sortedByFrequency.slice(0, 3) 
     return top3Words;
+}
+
+const getCleanInput = (inputText) => {
+    const inputTextCleaned = inputText.toLowerCase().trim();
+    const singleWord = inputTextCleaned.split(' ').pop();
+    const finalWord = singleWord.replace(/\d/g, '');
+    console.log('finalWord: ', finalWord)
+    return finalWord;
 }
--- src/util/predictiveWords.test.js
+++ src/util/predictiveWords.test.js
@@ -6,7 +6,7 @@ import { getWords } from './predictiveWords';
 import { exportAllDeclaration } from '@babel/types';
 
 describe('getWords', () => {
-  it.only('returns 3 predictive words sorted by the most frequent', () => {
+  it('returns 3 predictive words sorted by the most frequent', () => {
     const predictiveWords = getWords('a')
     
     expect(predictiveWords.length).toEqual(3);
@@ -18,14 +18,44 @@ describe('getWords', () => {
     expect(predictiveWords[2].count).toEqual(1);
   });
 
-  it.only('returns 0 words when no matching words found', () => {
+  it('returns 0 words when no matching words found', () => {
     const predictiveWords = getWords('c')
     
     expect(predictiveWords.length).toEqual(0);
   });
 
-  it.only('returns 0 words when nothing entered', () => {
+  it('returns 0 words when nothing entered', () => {
     const predictiveWords = getWords('')
+    
+    expect(predictiveWords.length).toEqual(0);
+  });
+
+  it('when multiple words entered, it only searches on the last word', () => {
+    const predictiveWords = getWords('hello a')
+    
+    expect(predictiveWords.length).toEqual(3);
+    expect(predictiveWords[0].word).toEqual('ac');
+    expect(predictiveWords[0].count).toEqual(3);
+    expect(predictiveWords[1].word).toEqual('ab');
+    expect(predictiveWords[1].count).toEqual(2);
+    expect(predictiveWords[2].word).toEqual('aa');
+    expect(predictiveWords[2].count).toEqual(1);
+  });
+
+  it('removes numbers from the search term', () => {
+    const predictiveWords = getWords('a1')
+    
+    expect(predictiveWords.length).toEqual(3);
+    expect(predictiveWords[0].word).toEqual('ac');
+    expect(predictiveWords[0].count).toEqual(3);
+    expect(predictiveWords[1].word).toEqual('ab');
+    expect(predictiveWords[1].count).toEqual(2);
+    expect(predictiveWords[2].word).toEqual('aa');
+    expect(predictiveWords[2].count).toEqual(1);
+  });
+
+  it('returns nothing when only numbers are entered', () => {
+    const predictiveWords = getWords('1234567')
     
     expect(predictiveWords.length).toEqual(0);
   });
